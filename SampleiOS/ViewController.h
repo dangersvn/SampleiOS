9c8a8c8cfff088a4bdb4062b0ffc388fc5093481
hai dang <haidang@hai-dangs-Mac-Pro.local>
2013-10-14 06:27:59 +0700

    Initial Commit

---
+++ SampleiOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleiOS
+//
+//  Created by hai dang on 10/14/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
